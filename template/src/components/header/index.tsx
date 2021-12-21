469e349ba2bc0d8f35413cdb7ee4d1d8c02e603e
Matthias Kluth <kluthmatthias@gmail.com>
2021-12-21 16:03:20 +0100

    feat: added a simple navbar and title

--- template/src/components/header/index.tsx
+++ template/src/components/header/index.tsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return (
         <header>
-            
+            <h1 className="text-3xl lg:text-5xl">
+                Kluth.me
+            </h1>
+            <nav>
+                <ul className="flex">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
