b83d56646345d64297dc9c5a1d3eaa2e87ec3e51
Matthias Kluth <kluthmatthias@gmail.com>
2021-12-29 10:55:33 +0100

    feat: deleted gravatar

--- template/src/components/home/index.tsx
+++ template/src/components/home/index.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import Gravatar from 'react-gravatar'
-
 export const Home = () => {
     return (
         <article>
-            <Gravatar md5='e6c6a20ca91121a25e76e4bb5b1aa0be' size={250} />
         </article>
     )
 }
